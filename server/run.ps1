db597f757253b5217d8d07321bffea9ee9feb8b1
seansie0830 <85049950+seansie0830@users.noreply.github.com>
2024-11-14 21:20:41 +0800

    Update run.ps1
    deployment script update

--- server/run.ps1
+++ server/run.ps1
@@ -1,19 +1,3 @@
-# 設定 Dockerfile 的路徑
-$dbDockerfilePath = "C:\Users\seans\codewhitelist\gdg\MonkeyWatchCopy\server\db.dockerfile"
-$webDockerfilePath = "C:\Users\seans\codewhitelist\gdg\MonkeyWatchCopy\server\web.dockerfile"
-
-# 設定 Docker image 的名稱
-$dbImageName = "monkeywatch-db"
-$webImageName = "monkeywatch-web"
-
-# 建置 db image
-docker build -t $dbImageName -f $dbDockerfilePath .
-
-# 執行 db container
-docker run -d --name monkeywatch-db-container $dbImageName
-
-# 建置 web image
-docker build -t $webImageName -f $webDockerfilePath .
-
-# 執行 web container，並連結到 db container
-docker run -d --name monkeywatch-web-container --link monkeywatch-db-container:db $webImageName
+docker build -t django-new -f web.dockerfile .
+docker build -t mysql -f db.dockerfile .
+docker-compose up -d
